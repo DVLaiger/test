9c2aaaf41ca75c9480bcc738fd6f743312a89767
DVLaiger <vladmegera96@gmail.com>
2021-08-15 20:35:11 +0300

    Init commit

--- header.php
+++ header.php
@@ -13,43 +13,30 @@
 
 <body <?php body_class(); ?> >
 <!--Пустой хэдер должен выглядить так, и меню примерно так-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">My first meny</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar fixed-top navbar-expand-lg mynavbar cf">
+    <div class="container-fluid">
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Catalog</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    To be investigated
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Contacts</a>
-                    <a class="dropdown-item" href="#">Purchase order</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Delivery</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Your demands</a>
-            </li>
-        </ul>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            <i class="fas fa-bars"></i>
+        </button>
+        <?php
+        wp_nav_menu( array(
+                'menu'              => 'Header menu',
+                'theme_location'    => 'header_menu',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_id'      => 'navbarNavDropdown',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'navbarContent',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+        );
+        ?>
+    </div>
 
-    </div>
-    <div class="container h-100">
-        <div class="d-flex justify-content-left h-100">
-            <div class="searchbar">
-                <input class="search_input" type="text" name="" placeholder="Search...">
-                <a href="#" class="search_icon"><i class="fa fa-search" aria-hidden="true"></i></a>
-            </div>
-        </div>
-    </div>
+    <?php echo get_search_form(); ?>
 </nav>
+
+
